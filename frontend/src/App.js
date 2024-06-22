75394947772c6d17a396fdbc4cef991611cbd1de
VST93380 <saitarunvysyaraju@gmail.com>
2024-06-22 18:31:25 +0530

    chore: Update App.js with new components and styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './styles/App.css';
+import Home from './components/Home';
+import React, { useEffect } from 'react';
+import Navbar from './components/Navbar';
+import ScrollToTop from './components/ScrollToTop';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {  Routes, Route } from 'react-router-dom';
+import Demo from './components/Demo';
+import { UserContext } from './UserContext';
+import { useLocation } from 'react-router-dom';
+import Authenticate from './components/Authenticate';
+import Form from './components/Form';
 function App() {
+  const location = useLocation(); // Use useLocation hook from react-router-dom
+
+  useEffect(() => {
+    const originalTitle = document.title;
+  
+    const handleBlur = () => {
+      document.title = "Come back 😢";
+    };
+  
+    const handleFocus = () => {
+      document.title = originalTitle;
+    };
+  
+    // Ensure correct event listener registration
+    window.addEventListener('blur', handleBlur);
+    window.addEventListener('focus', handleFocus);
+  
+    // Cleanup event listeners on component unmount
+    return () => {
+      window.removeEventListener('blur', handleBlur);
+      window.removeEventListener('focus', handleFocus);
+    };
+  }, []); // Empty dependency array ensures useEffect runs once on mount
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <>
+      <ToastContainer />
+      <ScrollToTop />
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Demo />} />
+      <Route path='/farmer' element={<Authenticate />} />
+      <Route path='/form' element={<Form />} />
+      <Route path='/volunteer' element={<Authenticate />} />
+      </Routes>
+      
+    </>
+    <></>
+    </>
   );
 }
 
