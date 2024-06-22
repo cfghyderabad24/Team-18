e9d84f644548d5339e5e9ea2f7202b349061e06e
Swaroop044 <dvvswaroop18@gmail.com>
2024-06-23 01:39:53 +0530

    updated cards.jsx

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -3,33 +3,35 @@ import { useNavigate } from 'react-router-dom';
 import './Cards.css'
 
 const Cards = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const cardsData = [
-    { id: 1, title: 'Data visualization with tables', icon: '📊', path: '/grid-example' },
-    { id: 2, title: 'Data visualization with charts', icon: '📈'  },
-    { id: 3, title: 'Respond to given queries', icon: '💬' , path : '/respond-to-query' },
-    { id: 4, title: 'Send a new message', icon: '✉️' , path: '/new-message'},
-    { id: 5, title: 'Add a new volunteer', icon: '👥' },
-    { id: 6, title: 'Add a new farmer', icon: '👩‍🌾' },
-  ];
+    const cardsData = [
+        { id: 1, title: 'Data visualization with tables', icon: '📊', path: '/grid-example' },
+        { id: 2, title: 'Data visualization with charts', icon: '📈' },
+        { id: 3, title: 'Respond to given queries', icon: '💬', path: '/respond-to-query' },
+        { id: 4, title: 'Send a new message', icon: '✉️', path: '/new-message' },
+        { id: 5, title: 'Add a new volunteer', icon: '👥' },
+        { id: 6, title: 'Add a new farmer', icon: '👩‍🌾' },
+    ];
 
-  const handleCardClick = (path) => {
-    if (path) {
-      navigate(path);
-    }
-  };
+    const handleCardClick = (path) => {
+        if (path) {
+            navigate(path);
+        }
+    };
 
-  return (
-    <div className="cards-container">
-      {cardsData.map((card) => (
-        <div key={card.id} className="card" onClick={() => handleCardClick(card.path)}>
-          <span className="card-icon">{card.icon}</span>
-          <span className="card-title">{card.title}</span>
-        </div>
-      ))}
-    </div>
-  );
+    return (
+        <>
+            <div className="cards-container">
+                {cardsData.map((card) => (
+                    <div key={card.id} className="card" onClick={() => handleCardClick(card.path)}>
+                        <span className="card-icon">{card.icon}</span>
+                        <span className="card-title">{card.title}</span>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default Cards;
