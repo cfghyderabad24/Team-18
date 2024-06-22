c7b4f9bc99c193764fc48906e3bf4fe9b25a0a02
VST93380 <saitarunvysyaraju@gmail.com>
2024-06-22 18:19:01 +0530

    chore: Add Authenticate component for user authentication

---
+++ frontend/src/components/Authenticate.jsx
@@ -0,0 +1,7 @@
+import React, { useState,useEffect } from "react";
+import bg from '../assets/bg.jpg';
+import axios from 'axios';
+import '../styles/login.css'
+import { useNavigate } from 'react-router-dom';
+import Alert from './Alert'; // Corrected import path
+import { Link } from "react-router-dom";
