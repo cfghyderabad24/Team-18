90bde8e0cbfaf92f05acc7fe89a0a4a9ad812cc5
VST93380 <saitarunvysyaraju@gmail.com>
2024-06-22 18:25:13 +0530

    Demo component added

---
+++ frontend/src/components/Demo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar2 from './Navbar2'; // Navbar2 for Demo component
+
+const Demo = () => {
+  return (
+    <div>
+      <Navbar2 /> {/* Render Navbar2 specifically for Demo component */}
+      <h1>Demo Component</h1>
+      {/* Add your Demo component content here */}
+    </div>
+  );
+};
+
+export default Demo;
