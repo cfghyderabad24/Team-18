386fc8dcfe3ab3605a06e3238d218dc4b5522fa3
VST93380 <saitarunvysyaraju@gmail.com>
2024-06-22 20:09:40 +0530

    refactor: Remove checkbox from Authenticate1 component

--- frontend/src/components/Authenticate1.jsx
+++ frontend/src/components/Authenticate1.jsx
@@ -100,8 +100,6 @@ const Authenticate = () => {
                   <h6 className="mb-0 pb-3">
                     <span style={{ color: 'white' }}>Log In </span>
                   </h6>
-                  <input className="checkbox" type="checkbox" id="reg-log" name="reg-log" />
-                  <label htmlFor="reg-log"></label>
                   <div className="card-3d-wrap mx-auto slide-fwd-center">
                     <div className="card-3d-wrapper">
                       <div className="card-front">
