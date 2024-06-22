eb77794c02c026741ec13cd9815c0fda20806f98
tejaanjani <anjanikatta2004@gmail.com>
2024-06-22 15:53:20 +0530

    Created a basic headline to ask a query

---
+++ Farmer/src/Components/Queries/Querypage.jsx
@@ -0,0 +1,11 @@
+const Querypage = () => {
+  return (
+    <div>
+      <div className="queries-page">
+        <h1>Ask Your Query</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Querypage;
