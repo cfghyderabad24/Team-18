413cba4f3d8df82a9c184c8cf8786c9d68ce28f1
tejaanjani <anjanikatta2004@gmail.com>
2024-06-22 15:55:05 +0530

    Created a text area to post the question

--- Farmer/src/Components/Queries/Querypage.jsx
+++ Farmer/src/Components/Queries/Querypage.jsx
@@ -3,6 +3,17 @@ const Querypage = () => {
     <div>
       <div className="queries-page">
         <h1>Ask Your Query</h1>
+        <form className="query-form">
+          <div className="form-group">
+            <label htmlFor="question">Your Question:</label>
+            <textarea
+              id="question"
+              name="question"
+              placeholder="Type your question here..."
+              required
+            ></textarea>
+          </div>
+        </form>
       </div>
     </div>
   );
